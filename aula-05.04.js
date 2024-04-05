81dc60dd461945589f84d18b07239dce241e58aa
Isaacmathias07 <133376387+Isaacmathias07@users.noreply.github.com>
2024-04-05 09:50:12 -0300

    Add files via upload

---
+++ aula-05.04.js
@@ -0,0 +1,33 @@
+/*
+let nome = "Isaac";
+let idade = 17;
+
+console.log(`Meu nome é ${nome} e minha idde é ${idade} anos`)
+*/
+
+
+//let num1 = 200
+//let num2 = 20
+
+//num1 = Number(num1);
+//num2 - Number(num2);
+
+//console.log(`A soma dos fatores é ${num1 + num2}`)
+//console.log(`A divisão dos fatores é ${num1 / num2}`)
+//console.log(`A subtração dos fatores é ${num1 - num2} `)
+//console.log(`A multiplicação dos fatores é ${num1 * num2} `)
+
+/*let idade = 20
+idade = Number(idade);
+
+if (idade >= 18) {
+    console.log("Voce é maior de idade.");
+}
+else {
+    console.log("Voce é menor de idade.");
+}
+*/
+
+for (let numero = 0; numero <= 100; numero++) {
+    console.log(numero);
+}
